4e2eabeddf0577611ce0dc33aabfff8418fcfefd
LSanchez08 <luis.alberto.sanchez.rios@live.com.mx>
2021-03-16 18:47:45 -0600

    exercise 1

--- Lui/Basics/cycles.py
+++ Lui/Basics/cycles.py
@@ -1,6 +1,6 @@
 
 print('*********************************************************')
-if (4 < 5):
+if 4 < 5:
     print(True)
 else:
     print(False)
@@ -13,7 +13,7 @@ else:
 a = 10
 b = 5
 
-if (a + b == 3):
+if a + b == 3:
     print(1)
 elif (a + b == 15):
     print(0)
@@ -23,15 +23,15 @@ else:
 print('*********************************************************')
 
 cont = 0
-while (cont < 10):
+while cont < 10:
     cont += 1
 
     print(cont)
 
 cont = 0
-while (cont < 10):
+while cont < 10:
     cont += 1
-    if (cont == 5):
+    if cont == 5:
         continue
 
     print(cont)
@@ -39,7 +39,7 @@ while (cont < 10):
 cont = 0
 while cont < 10:
     cont += 1
-    if (cont == 5):
+    if cont == 5:
         break
     
     print(cont)
@@ -49,3 +49,42 @@ print('*********************************************************')
 for i in range(10):
     print(i)
 
+print("----------------------------")
+
+for i in range(5, 10):
+    print(i)
+
+print("----------------------------")
+
+for i in range(0, 10, 2):
+    print(i)
+
+print("----------------------------")
+
+for i in range(10, 0, -1):
+    print(i)
+
+print('*********************************************************')
+
+List = [1,2,3,4,5]
+
+for x in List:
+    print(x)
+    # print(List.index(x))
+
+
+print('*********************************************************')
+a = 100
+
+b = 0
+
+try:
+    print(a * b)
+except:
+    print('error')
+
+
+try:
+    print(a/b)
+except:
+    print('error')
---
+++ Lui/Basics/functions.py
@@ -0,0 +1,32 @@
+
+def MyFunction():
+    pass
+
+def sumar(a, b):
+    return a + b
+
+print(sumar(1, 2))
+
+c = sumar(10, 30)
+
+print(c)
+
+def imprimir(data):
+    print(data)
+
+imprimir('rafa es puto')
+imprimir(True)
+
+
+def Funcion():
+    List.sort()
+
+List = [6, 3, 8, 4, 6, 7]
+a = 20
+
+print(List)
+
+Funcion()
+
+print(List)
+
